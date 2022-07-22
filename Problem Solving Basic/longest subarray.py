ada3455b4eb5e2207149be6955149bb4d2e32eb0
Aditi Kunwar <88850715+AditiKunwar@users.noreply.github.com>
2022-07-22 20:01:13 +0530

    Update longest subarray.py

--- Problem Solving Basic/longest subarray.py
+++ Problem Solving Basic/longest subarray.py
@@ -4,13 +4,9 @@ import random
 import re
 import sys
 
-
-#
 # Complete the 'longestSubarray' function below.
-#
 # The function is expected to return an INTEGER.
 # The function accepts INTEGER_ARRAY arr as parameter.
-#
 
 def longestSubarray(arr):
     n = len(arr)
